35eedbd3692ed28b252abe584dda4da6387da172
ripee <ripee.dev@gmail.com>
2020-10-17 20:10:51 +0200

    trlte-common: Uprev keymaster hal to 4.0

--- trlte.mk
+++ trlte.mk
@@ -190,8 +190,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
